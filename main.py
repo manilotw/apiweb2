c8e4f5329416174c3c88cec5870a99d6e605a184
Amirzhan Albariyev <albariev123@gmail.com>
2024-09-22 00:04:22 +0300

    Add fixes

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import requests
 from urllib.parse import urlparse
 from environs import Env
 
-env = Env()
-env.read_env()
 
-token = env.str('token')
-
-def shorten_link(token, url):
+def shorten_link(token,
+                  url):
     params = {
         'access_token': token,
         'url': url,
@@ -15,14 +12,16 @@ def shorten_link(token, url):
     }
     response = requests.get('https://api.vk.com/method/utils.getShortLink', params=params)
     response.raise_for_status()
-    data = response.json()
+    json_data = response.json()
 
-    if 'response' not in data:
+    if 'response' not in json_data:
         raise requests.exceptions.HTTPError('Ошибка при сокращении ссылки')
 
-    return data['response']['short_url']
+    return json_data['response']['short_url']
 
-def count_clicks(token, url):
+
+def count_clicks(token,
+                  url):
     params = {
         'access_token': token,
         'key': urlparse(url).path.strip('/'),
@@ -30,27 +29,37 @@ def count_clicks(token, url):
     }
     response = requests.get('https://api.vk.com/method/utils.getLinkStats', params=params)
     response.raise_for_status()
-    data = response.json()
+    json_data = response.json()
 
-    if 'response' not in data:
+    if 'response' not in json_data:
         raise requests.exceptions.HTTPError('Ошибка при получении статистики')
 
-    return data['response']['stats'][0]['views']
+    return json_data['response']['stats'][0]['views']
+
 
 def is_shorten_link(url):
     return urlparse(url).netloc == 'vk.cc'
 
+
 def main():
+    env = Env()
+    env.read_env()
+
+    vk_token = env.str('VK_TOKEN')
     url = input('Введите ссылку: ')
 
     try:
         if is_shorten_link(url):
-            clicks = count_clicks(token, url)
+            clicks = count_clicks(vk_token,
+                                   url)
             print('Количество посещений:', clicks)
         else:
-            short_url = shorten_link(token, url)
+            short_url = shorten_link(vk_token,
+                                      url)
             print('Короткая ссылка:', short_url)
     except requests.exceptions.HTTPError as e:
         print('Ошибка:', e)
 
-main()
+
+if __name__ == "__main__":
+    main()
